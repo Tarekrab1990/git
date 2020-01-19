6f24b23ea8010b580a3fb0b75b9097dc7ebd1bb8
tarek <hadil20131@gmail.com>
2020-01-19 14:58:20 +0100

    changement avec master

--- index.php
+++ index.php
@@ -7,8 +7,5 @@
 <footer>
     <main>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque, et alias assumenda, sequi culpa neque nam perferendis esse incidunt animi placeat omnis quae inventore molestiae error voluptate magnam unde. Velit!</main>
 </footer>
-<<<<<<< HEAD
 <p>modified with master</p>
-=======
 <p>modified with dev</p>
->>>>>>> dev
